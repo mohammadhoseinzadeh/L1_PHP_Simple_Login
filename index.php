e6354372fc3cc05fc08d2821d9207a79d420f220
mohammadelendil <mohammadapp1989@gmail.com>
2024-04-09 07:30:47 +0330

    first commit

---
+++ index.php
@@ -0,0 +1,199 @@
+<?php
+session_start();
+//$coler=['red' ,'green','blue'];
+//echo'<pre>';
+//$age= null;
+//$str= 'hello how are you';
+//$str= explode(' ',$str);
+
+//$coler=['red' ,'green','blue'];
+//$coler= implode(',',$coler);
+
+//echo'<pre>';
+//var_dump($coler);
+//echo $coler;
+
+//$coment= 'ok khili khob bod';
+//$coment= '<script>alert("brrrrr")</script>';
+
+// $coler=['red' ,'green','blue'];
+// $coler= join(',',$coler);
+
+// //echo htmlspecialchars($coler);
+// echo htmlentities($coler);
+
+// $price=11111111110000000000000000000;
+// echo number_format($price);
+
+// $str ='mohammad';
+// echo strlen($str);
+// $name='mohammad';
+// $str= "hi miol gdysjh $name  monf";
+
+// $coler=[
+//     ['one'=> 'red' ,'two'=> 'green','three'=> 'blue'],
+//     ['one'=> 'black' ,'two'=> 'green','three'=> 'blue'],
+//     ['one'=> 'red' ,'two'=> 'green','three'=> 'blue'],
+// ];
+
+// $car = ['bmw','benz','pride'];
+// $price= [400,1222,60];
+// $str= array_combine($car,$price);
+// $str=['one'=> 'red' ,'two'=> 'green','three'=> 'blue'];
+// echo'<pre>';
+// var_dump(array_values ($str));
+// $name='ahmad';
+// $lost='dfgy';
+// $age=32;
+// $arr = compact('name','lost','age');
+// $car = ['bmw','benz','pride'];
+// echo'<pre>';
+// var_dump(in_array('bmw',$car));
+// echo in_array('bmw',$car);
+// $str='ldaskvheruinwnjfiwehfncewcn';
+// echo substr($str,0,10). ' .8..';
+// $name = null ;
+// echo $name = $name ?? "-";
+// $age = 7 ;
+// if ($age>=10)
+// {
+//     echo'ok';
+// }
+// else{
+//     echo'no';
+// }
+
+
+// $point = 0;
+
+// if ($point >= 15 && $point <= 20) {
+//     echo 'Grade A';
+// } elseif ($point >= 10 && $point < 15) {
+//     echo 'Grade B';
+// } elseif ($point >= 5 && $point < 10) {
+//     echo 'Grade C';
+// } elseif ($point >= 0 && $point < 5) {
+//     echo 'Grade D';
+// } else {
+//     echo 'error';
+// }
+
+// $point = 0;
+
+// switch ($point) {
+//     case ($point == 20):
+//         echo 'best';
+//         break;
+//     case $point == 15:
+//         echo 'no best';
+//         break;
+//     default:
+//         echo 'no no no';
+// }
+
+// $counter = 7;
+
+// while($counter <= 5){
+//     echo 'hassan' . '<br/>';
+//     $counter++;
+// }
+
+
+// $counter =6;
+// do {
+//     echo 'hassan' . '<br/>';
+//     $counter++;
+// } while ($counter <= 5);
+
+// for ($i = 1; $i <= 5; $i++) {
+//     echo $i . '<br/>';
+// }
+
+//$colors = ['red', 'blue', 'green'];
+// $colors = ['a' => 'red', 'b' => 'blue', 'c' => 'green'];
+
+// for ($i = 0; $i < count($colors); $i++) {
+//     echo $colors[$i] . '<br/>';
+// }
+// foreach ($colors as $color) {
+//     echo $color  . '<br/>';
+// }
+
+// foreach ($colors as $key => $color) {
+//     echo $key . ':' .  $color  . '<br/>';
+// }
+
+
+// $i = 1;
+// while ($i <= 10) {
+//     if($i === 5){
+//         break;
+//     }
+//     echo $i;
+// }
+// for ($i = 1; $i <= 10; $i++) {
+//     if ($i === 5) {
+//         break;
+//     }
+//     echo $i;
+// }
+// for ($i = 1; $i <= 10; $i++) {
+//     if ($i === 5) {
+//         continue;
+//     }
+//     echo $i;
+// }
+
+// function add($num1, $num2 )
+// {
+//     echo $num1 + $num2 ;
+
+// }
+// add(45,67);
+// function addTen(&$num)
+// {
+//     $num += 10;
+// }
+// $number = 40 ;
+// addTen($number);
+// echo $number;
+// $a = 10 ;
+// function myFunc()
+// {
+//     global $a ;
+//     echo $a ;
+
+// }
+// myFunc();
+
+// function myFunc()
+// {
+//     static $a = 0 ;
+//     echo $a ;
+//     $a++ ;
+// }
+
+// myFunc();
+// myFunc();
+
+// function add($num1, $num2 = 10)
+// {
+//     return $num1 + $num2 ;
+// }
+// // echo add(23,30);
+// echo add(23);
+echo'<pre>';
+// var_dump($_SERVER['MIBDIRS']);
+// $_SESSION['fName'] = 'MOHAMMAD';
+// setcookie('fName','mohammadHZ',time() - (86400 * 30));
+
+
+
+
+
+
+
+
+
+
+
---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!-- <form action="login-store.php" method="get"> -->
+<form action="login-store.php" method="post">
+
+    <label for="email">Email</label>
+    <input type="text" id="email"  name="email">
+    <br>
+    <br>
+    <label for="password">Password</label>
+    <input type="text" id="password" name="password">
+    <br>
+    <br>
+    <button type="submit">submit</button>
+</form>
+</body>
+</html>
